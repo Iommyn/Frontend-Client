6e894368a5c01005f67d9e41f32555ff74560580
Iommy <VunixNetwork@gmail.com>
2024-03-03 12:58:54 +0300

    глобальные изменения

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Section from "../components/MainPage/section/Section";
 import NavbarApp from "../components/Navbar/Navbar";
 import FooterApp from "../components/Footer/Footer";
 import React from "react";
+import s from '../pages/auth/auth.module.css'
 const Home = () => {
 
     return (
